d746910f388c8b1d4e165f988aac66c51fe9f72b
mhurchaidh <bmurchie1997@gmail.com>
2023-02-22 18:24:59 -0800

    commit tree e6dbbbb29667a7ba3dea7221b0d46603b0e40988

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,35 @@
+class SpicesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+    def index
+        render json: Spice.all
+    end
+
+    def create
+        render json: Spice.create(spice_params), status: :created
+    end
+
+    def update
+        spice = find_spice
+        spice.update(spice_params)
+        render json: spice
+    end
+
+    def destroy
+        spice = find_spice
+        spice.destroy
+    end
+
+    private
+    def find_spice
+        Spice.find(params[:id])
+    end
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    def render_not_found
+        render json: {error: 'Spice not found'}, status: :not_found
+    end
+end
